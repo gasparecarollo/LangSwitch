e31e7b9f812637a8d39e2e034296347c13efd771
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-06-18 13:20:36 -0400

    Added languages, wrapped app in i18nextProvider inside index.js, created WeatherApp.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+i18next.init({
+  interpolation: { escapeValue: false },
+});
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
